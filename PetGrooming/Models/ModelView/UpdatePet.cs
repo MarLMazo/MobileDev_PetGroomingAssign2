4bec6f41286ff42cca1ac7b32750a43d7d920f05
MarlMazo <marlmazo01@gmail.com>
2020-01-30 18:32:39 -0500

    Added UpdatePet ModelView class

---
+++ PetGrooming/Models/ModelView/UpdatePet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetGrooming.Models.ModelView
+{
+    public class UpdatePet
+    {
+        public Pet Pet { get; set; }
+
+        public ICollection<Species> Species { get; set; }
+    }
+}
